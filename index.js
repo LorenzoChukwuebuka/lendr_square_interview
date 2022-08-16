4e1954007a559750cb171a72871a37b753bb4f35
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2022-08-16 16:24:28 +0100

    done

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const helmet = require('helmet')
 const userRoutes = require('./routes/userRoutes')
 
 const knex = require('knex')
-
 const knexFile = require('./knexfile').development
-
 const db = knex(knexFile)
 
 app.use(helmet())
@@ -18,8 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', userRoutes)
-app.use('/',(req,res)=>{
-    res.send('<h4> Welcome to the app </h4>')
+app.use('/', (req, res) => {
+  res.send('<h4> Welcome to the app </h4>')
 })
 
 const PORT = process.env.PORT || 8081
